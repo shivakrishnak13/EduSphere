579ee00a843ab3a6e97ca697bd9a0e213ee1aa9a
shivakrishnak13 <kosarishivakrishna@gmail.com>
2023-09-03 21:01:53 +0530

    Assigment Update Page

--- Frontend/src/app/instructorsingleassign/instructorsingleassign.component.ts
+++ Frontend/src/app/instructorsingleassign/instructorsingleassign.component.ts
@@ -5,6 +5,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { GlobalService } from '../global.service';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { ConfirmationService, MessageService, ConfirmEventType } from 'primeng/api';
+import { DatePipe } from '@angular/common';
 
 
 @Component({
@@ -20,9 +21,17 @@ export class InstructorsingleassignComponent implements OnInit {
   title: string ="";
   due_date : string =""
 
-  constructor(private http: HttpClient, private route: ActivatedRoute, private globalService: GlobalService, private sanitizer: DomSanitizer, private messageService: MessageService, private confirmationService: ConfirmationService,private router: Router) { }
+  constructor(private http: HttpClient, private route: ActivatedRoute, private globalService: GlobalService, private sanitizer: DomSanitizer, private messageService: MessageService, private confirmationService: ConfirmationService,private router: Router,private datePipe: DatePipe) { }
 
   ngOnInit(): void {
+   
+    this.getAllAssignemnts()
+
+
+  }
+
+
+  getAllAssignemnts(){
     const token = this.globalService.getInstructorLoginDetails()?.token ?? '';
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
@@ -36,7 +45,6 @@ export class InstructorsingleassignComponent implements OnInit {
       this.text = res?.description
 
     })
-
   }
 
   sanitizeAndTrustHtml(html: string): SafeHtml {
@@ -86,6 +94,28 @@ export class InstructorsingleassignComponent implements OnInit {
     });
   }
 
+  handleAssignmentUpdate(id:number){
+    const token = this.globalService.getInstructorLoginDetails()?.token ?? '';
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': token
+    });
+    let payload = {
+      title : this.title,
+      due_date : this.datePipe.transform(this.due_date, 'yyyy-MM-dd'),
+      description  :this.text
+    }
+    this.http.patch(`${environment.API_URL}/api/assignment/${id}`,payload,{headers}).subscribe((res:any)=>{
+      this.messageService.add({ severity: 'success', summary: 'Updated', detail: res.message });
+      this.getAllAssignemnts()
+        this.isEdit = true
+    })
+
+     
+    
+    
+    
+  }
 
 
 
