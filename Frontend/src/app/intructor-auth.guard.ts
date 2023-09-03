f7ddf2380471a766caf1b0bdc2b91ae5c203b8bf
shivakrishnak13 <kosarishivakrishna@gmail.com>
2023-09-03 11:20:29 +0530

    Instructor Authentication

--- Frontend/src/app/intructor-auth.guard.ts
+++ Frontend/src/app/intructor-auth.guard.ts
@@ -6,9 +6,23 @@ import { GlobalService } from './global.service';
 @Injectable({
   providedIn: 'root'
 })
+export class InstructorAuthGuard implements CanActivate {
+  constructor(private globalService: GlobalService, private router: Router) {}
 
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    // Implement your authentication logic here
+    if (this.globalService.isInstructorLogin) {
+      return true; // Allow access for instructors
+    } else {
+      // Redirect to the login page if not authenticated
+      this.globalService.redirectUrl = state.url;
 
-export const intructorAuthGuard implements CanActivate{
-
- 
-};
+      // Redirect to the login page
+      return this.router.parseUrl('/instructor/signin');
+      // return this.router.createUrlTree(['/instructor/signin']);
+    }
+  }
+}
